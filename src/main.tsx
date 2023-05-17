460ffe62fe91bc6957710de945b60f32815ff728
gordiviris <ksandrade@email.neit.edu>
2023-05-16 21:08:39 -0400

    class activity

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.js';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProductsList } from './components/products/ProductsList';
 import { ProductDetail } from './components/products/ProductDetail';
 import { RouteNotFound } from './components/RouteNotFound';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
